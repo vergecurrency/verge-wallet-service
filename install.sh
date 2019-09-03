0d719c23728c3860b442249e52d608ee0c245a9c
Swen van Zanten <info@hdsign.nl>
2019-09-03 23:03:29 +0200

    Add submodules

--- install.sh
+++ install.sh
@@ -1,10 +1,2 @@
 # Create an environment file
 cp env.example .env
-
-# Get bitcore
-git clone https://github.com/vergecurrency/bitcore.git
-cd bitcore
-cd ..
-
-# Get Verged
-git clone https://github.com/vergecurrency/vDocker.git
